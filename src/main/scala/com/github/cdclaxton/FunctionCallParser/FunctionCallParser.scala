192d050bc1911bbf34fc192c7c6b1fdeb2442ee6
Chris <c.d.claxton@gmail.com>
2017-05-25 10:35:10 +0100

    Initial commit

---
+++ src/main/scala/com/github/cdclaxton/FunctionCallParser/FunctionCallParser.scala
@@ -0,0 +1,191 @@
+package com.github.cdclaxton.FunctionCallParser
+
+import org.apache.commons.lang3.StringEscapeUtils.escapeJava
+
+object ParameterType extends Enumeration {
+  type ParameterType = Value
+  val QUOTED_STRING, LITERAL, NUMERIC = Value
+}
+
+case class ParsedFunctionCall(functionName: String,
+                              params: Seq[Parameter])
+
+case class Parameter(tpe: ParameterType.Value,
+                     value: String)
+
+object FunctionCallParser {
+
+  object State extends Enumeration {
+    type State = Value
+    val START, FUNCTION_NAME, OPENING_BRACKET, WHITESPACE,
+    PARAM_QUOTED_OPEN, PARAM_QUOTED_CLOSED, PARAM_QUOTED_INNER,
+    PARAM_LITERAL, PARAM_NUMERIC,
+    ESCAPED_CHAR, COMMA, CLOSE_BRACKET, INVALID = Value
+  }
+
+  private[FunctionCallParser] def start(c: Char): State.Value = {
+    if (!Character.isAlphabetic(c)) State.INVALID
+    else State.FUNCTION_NAME
+  }
+
+  private[FunctionCallParser] def inFunctionName(c: Char): State.Value = {
+    if (Character.isAlphabetic(c) || Character.isDigit(c)) State.FUNCTION_NAME
+    else if (c == '(') State.OPENING_BRACKET
+    else State.INVALID
+  }
+
+  private[FunctionCallParser] def inOpeningBracket(c: Char): State.Value = {
+    if (Character.isWhitespace(c)) State.WHITESPACE
+    else if (c == ')') State.CLOSE_BRACKET
+    else inStartParam(c)  // maybe it's the start of a parameter?
+  }
+
+  private[FunctionCallParser] def inWhitespace(c: Char): State.Value = {
+    if (c == ',') State.COMMA
+    else if (Character.isWhitespace(c)) State.WHITESPACE
+    else if (c == ')') State.CLOSE_BRACKET
+    else inStartParam(c)  // maybe it's the start of a parameter?
+  }
+
+  private[FunctionCallParser] def inStartParam(c: Char): State.Value = {
+    if (c == '"') State.PARAM_QUOTED_OPEN
+    else if (Character.isDigit(c) || c == '.') State.PARAM_NUMERIC
+    else if (Character.isAlphabetic(c)) State.PARAM_LITERAL
+    else State.INVALID
+  }
+
+  private[FunctionCallParser] def inParamQuotedOpen(c: Char): State.Value = {
+    if (c == '"') State.PARAM_QUOTED_CLOSED
+    else if (c == '\\') State.ESCAPED_CHAR
+    else State.PARAM_QUOTED_INNER
+  }
+
+  private[FunctionCallParser] def inParamQuotedInner(c: Char): State.Value = {
+    if (c == '"') State.PARAM_QUOTED_CLOSED
+    else if (c == '\\') State.ESCAPED_CHAR
+    else State.PARAM_QUOTED_INNER
+  }
+
+  private[FunctionCallParser] def inParamQuotedClosed(c: Char): State.Value = {
+    if (Character.isWhitespace(c)) State.WHITESPACE
+    else if (c == ',') State.COMMA
+    else if (c == ')') State.CLOSE_BRACKET
+    else State.INVALID
+  }
+
+  private[FunctionCallParser] def inEscapedChar(c: Char): State.Value = {
+    State.PARAM_QUOTED_INNER
+  }
+
+  private[FunctionCallParser] def inParamNumeric(c: Char): State.Value = {
+    if (Character.isWhitespace(c)) State.WHITESPACE
+    else if (c == ',') State.COMMA
+    else if (Character.isDigit(c) || c == '.') State.PARAM_NUMERIC
+    else if (c == ')') State.CLOSE_BRACKET
+    else State.INVALID
+  }
+
+  private[FunctionCallParser] def inParamLiteral(c: Char): State.Value = {
+    if (Character.isWhitespace(c)) State.WHITESPACE
+    else if (Character.isAlphabetic(c) || Character.isDigit(c)) State.PARAM_LITERAL
+    else if (c == ')') State.CLOSE_BRACKET
+    else State.INVALID
+  }
+
+  private[FunctionCallParser] def inComma(c: Char): State.Value = {
+    if (Character.isWhitespace(c)) State.WHITESPACE
+    else inStartParam(c)  // maybe it's the start of a parameter?
+  }
+
+  private[FunctionCallParser] def endOfParameter(state: State.Value) = {
+    state == State.CLOSE_BRACKET ||
+      state == State.WHITESPACE ||
+      state == State.COMMA
+  }
+
+
+
+  def parseFunctionCall(functionCall: String): Option[ParsedFunctionCall] = {
+
+    // Get the length of the function call
+    val len = functionCall.length
+
+    // If the length is insufficient for a valid call, then return.
+    if (len < 3) return None
+
+    // Break up the string into an array of characters so they can be walked over
+    val arrayOfChars: Array[Char] = functionCall.toCharArray
+
+    // Initialisation of stateful variables
+    var prevState = State.INVALID
+    var currentState = State.INVALID
+    var c: Char = 'a'  // dummy char just for initialisation purposes
+    var ptr: Int = 0
+    var parsedFunctionCall = ParsedFunctionCall("", List[Parameter]())
+
+    // Walk through each character
+    for (i <- 0 until len) {
+
+      prevState = currentState
+      c = arrayOfChars(i)  // get the i(th) character
+
+      // Find the new 'state' of the state machine based on the previous state and the i(th) character
+      currentState = if (i == 0) start(c)
+      else if (prevState == State.FUNCTION_NAME) inFunctionName(c)
+      else if (prevState == State.OPENING_BRACKET) inOpeningBracket(c)
+      else if (prevState == State.WHITESPACE) inWhitespace(c)
+      else if (prevState == State.COMMA) inComma(c)
+      else if (prevState == State.PARAM_QUOTED_OPEN) inParamQuotedOpen(c)
+      else if (prevState == State.PARAM_QUOTED_INNER) inParamQuotedInner(c)
+      else if (prevState == State.PARAM_QUOTED_CLOSED) inParamQuotedClosed(c)
+      else if (prevState == State.ESCAPED_CHAR) inEscapedChar(c)
+      else if (prevState == State.PARAM_LITERAL) inParamLiteral(c)
+      else if (prevState == State.PARAM_NUMERIC) inParamNumeric(c)
+      else State.INVALID
+
+      println("STATE: " + currentState)
+
+      // End of function name
+      if (currentState == State.OPENING_BRACKET && prevState == State.FUNCTION_NAME) {
+        parsedFunctionCall = parsedFunctionCall.copy(functionName = functionCall.substring(0, i))
+        println("Got function name: " + parsedFunctionCall)
+      }
+
+      // Start of a parameter
+      if ((currentState == State.PARAM_LITERAL || currentState == State.PARAM_NUMERIC || currentState == State.PARAM_QUOTED_OPEN) &&
+        (!(prevState == State.PARAM_LITERAL || prevState == State.PARAM_NUMERIC || prevState == State.PARAM_QUOTED_OPEN))) {
+        ptr = i
+      }
+
+      // End of a parameter
+      if (endOfParameter(currentState)) {
+        val parameterString = functionCall.substring(ptr, i)
+
+        val param: Option[Parameter] = if (prevState == State.PARAM_LITERAL) Some(Parameter(ParameterType.LITERAL, parameterString))
+          else if (prevState == State.PARAM_NUMERIC) Some(Parameter(ParameterType.NUMERIC, parameterString))
+          else if (prevState == State.PARAM_QUOTED_CLOSED) {
+            val str = escapeJava(parameterString.substring(1, parameterString.length-1))
+            Some(Parameter(ParameterType.QUOTED_STRING, str))
+          }
+
+          else None
+
+        if (param.isDefined) parsedFunctionCall = parsedFunctionCall.copy(params = parsedFunctionCall.params ++ List(param.get))
+      }
+
+      // End of function call
+      if (currentState == State.CLOSE_BRACKET) {
+        println("Returning: " + parsedFunctionCall)
+        return Some(parsedFunctionCall)
+      }
+
+      // Something went wrong with the parsing ...
+      if (currentState == State.INVALID) {
+        println("INVALID STATE")
+        return None
+      }
+    }
+
+    None
+  }
+}
---
+++ src/test/scala/com/github/cdclaxton/FunctionCallParser/FunctionCallParserTest.scala
@@ -0,0 +1,266 @@
+package com.github.cdclaxton.FunctionCallParser
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.{FlatSpec, Matchers}
+
+@RunWith(classOf[JUnitRunner])
+class FunctionCallParserTest extends FlatSpec with Matchers {
+
+  // No arguments
+  // -------------------------------------------------------------------------------------------------------------------
+
+  "parseFunctionCall()" should "work correctly when there aren't any parameters" in {
+    val result: Option[ParsedFunctionCall] = FunctionCallParser.parseFunctionCall("""f()""")
+    result.isDefined should be (true)
+    result.get should be (ParsedFunctionCall(functionName = "f", params = Nil))
+  }
+
+  it should "work correctly when there aren't any parameters and the function name has two characters" in {
+    val result: Option[ParsedFunctionCall] = FunctionCallParser.parseFunctionCall("""fn()""")
+    result.isDefined should be (true)
+    result.get should be (ParsedFunctionCall(functionName = "fn", params = Nil))
+  }
+
+  it should "work correctly when there aren't any parameters and there is trailing whitespace" in {
+    val result: Option[ParsedFunctionCall] = FunctionCallParser.parseFunctionCall("""fn()  """)
+    result.isDefined should be (true)
+    result.get should be (ParsedFunctionCall(functionName = "fn", params = Nil))
+  }
+
+  it should "work correctly when the function name contains a digit" in {
+    val result: Option[ParsedFunctionCall] = FunctionCallParser.parseFunctionCall("""f1()""")
+    result.isDefined should be (true)
+    result.get should be (ParsedFunctionCall(functionName = "f1", params = Nil))
+  }
+
+  // Numeric-only arguments
+  // -------------------------------------------------------------------------------------------------------------------
+
+  it should "handle a single numeric value" in {
+    val result: Option[ParsedFunctionCall] = FunctionCallParser.parseFunctionCall("""fn(1)""")
+    result.isDefined should be (true)
+    result.get should be (ParsedFunctionCall(functionName = "fn",
+      params = Seq(Parameter(tpe = ParameterType.NUMERIC, value = "1"))))
+  }
+
+  it should "handle a double digit numeric value" in {
+    val result: Option[ParsedFunctionCall] = FunctionCallParser.parseFunctionCall("""fn(12)""")
+    result.isDefined should be (true)
+    result.get should be (ParsedFunctionCall(functionName = "fn",
+      params = Seq(Parameter(tpe = ParameterType.NUMERIC, value = "12"))))
+  }
+
+  it should "handle a triple digit numeric value" in {
+    val result: Option[ParsedFunctionCall] = FunctionCallParser.parseFunctionCall("""fn(123)""")
+    result.isDefined should be (true)
+    result.get should be (ParsedFunctionCall(functionName = "fn",
+      params = Seq(Parameter(tpe = ParameterType.NUMERIC, value = "123"))))
+  }
+
+  it should "handle a number preceded with a single whitespace character" in {
+    val result: Option[ParsedFunctionCall] = FunctionCallParser.parseFunctionCall("""fn( 123)""")
+    result.isDefined should be (true)
+    result.get should be (ParsedFunctionCall(functionName = "fn",
+      params = Seq(Parameter(tpe = ParameterType.NUMERIC, value = "123"))))
+  }
+
+  it should "handle a number preceded with two whitespace characters" in {
+    val result: Option[ParsedFunctionCall] = FunctionCallParser.parseFunctionCall("""fn(  123)""")
+    result.isDefined should be (true)
+    result.get should be (ParsedFunctionCall(functionName = "fn",
+      params = Seq(Parameter(tpe = ParameterType.NUMERIC, value = "123"))))
+  }
+
+  it should "handle a number with a trailing whitespace character" in {
+    val result: Option[ParsedFunctionCall] = FunctionCallParser.parseFunctionCall("""fn(123 )""")
+    result.isDefined should be (true)
+    result.get should be (ParsedFunctionCall(functionName = "fn",
+      params = Seq(Parameter(tpe = ParameterType.NUMERIC, value = "123"))))
+  }
+
+  it should "handle a number with two trailing whitespace characters" in {
+    val result: Option[ParsedFunctionCall] = FunctionCallParser.parseFunctionCall("""fn(123  )""")
+    result.isDefined should be (true)
+    result.get should be (ParsedFunctionCall(functionName = "fn",
+      params = Seq(Parameter(tpe = ParameterType.NUMERIC, value = "123"))))
+  }
+
+  it should "handle a function with two numeric arguments" in {
+    val result: Option[ParsedFunctionCall] = FunctionCallParser.parseFunctionCall("""fn(1,2)""")
+    result.isDefined should be (true)
+    result.get should be (ParsedFunctionCall(functionName = "fn",
+      params = Seq(
+        Parameter(tpe = ParameterType.NUMERIC, value = "1"),
+        Parameter(tpe = ParameterType.NUMERIC, value = "2"))))
+  }
+
+  it should "handle a function with two numeric arguments separated with whitespace" in {
+    val result: Option[ParsedFunctionCall] = FunctionCallParser.parseFunctionCall("""fn(1, 2)""")
+    result.isDefined should be (true)
+    result.get should be (ParsedFunctionCall(functionName = "fn",
+      params = Seq(
+        Parameter(tpe = ParameterType.NUMERIC, value = "1"),
+        Parameter(tpe = ParameterType.NUMERIC, value = "2"))))
+  }
+
+  it should "handle a function with three numeric arguments" in {
+    val result: Option[ParsedFunctionCall] = FunctionCallParser.parseFunctionCall("""fn(1,2,3)""")
+    result.isDefined should be (true)
+    result.get should be (ParsedFunctionCall(functionName = "fn",
+      params = Seq(
+        Parameter(tpe = ParameterType.NUMERIC, value = "1"),
+        Parameter(tpe = ParameterType.NUMERIC, value = "2"),
+        Parameter(tpe = ParameterType.NUMERIC, value = "3"))))
+  }
+
+  // Literal arguments
+  // -------------------------------------------------------------------------------------------------------------------
+
+  it should "handle a literal composed of a single character" in {
+    val result: Option[ParsedFunctionCall] = FunctionCallParser.parseFunctionCall("""fn(a)""")
+    result.isDefined should be (true)
+    result.get should be (ParsedFunctionCall(functionName = "fn",
+      params = Seq(Parameter(tpe = ParameterType.LITERAL, value = "a"))))
+  }
+
+  it should "handle a literal composed of two characters" in {
+    val result: Option[ParsedFunctionCall] = FunctionCallParser.parseFunctionCall("""fn(ab)""")
+    result.isDefined should be (true)
+    result.get should be (ParsedFunctionCall(functionName = "fn",
+      params = Seq(Parameter(tpe = ParameterType.LITERAL, value = "ab"))))
+  }
+
+  it should "handle a literal composed of two characters with leading whitespace" in {
+    val result: Option[ParsedFunctionCall] = FunctionCallParser.parseFunctionCall("""fn( ab)""")
+    result.isDefined should be (true)
+    result.get should be (ParsedFunctionCall(functionName = "fn",
+      params = Seq(Parameter(tpe = ParameterType.LITERAL, value = "ab"))))
+  }
+
+  it should "handle a literal composed of two characters with trailing whitespace" in {
+    val result: Option[ParsedFunctionCall] = FunctionCallParser.parseFunctionCall("""fn(ab )""")
+    result.isDefined should be (true)
+    result.get should be (ParsedFunctionCall(functionName = "fn",
+      params = Seq(Parameter(tpe = ParameterType.LITERAL, value = "ab"))))
+  }
+
+  // Quoted arguments
+  // -------------------------------------------------------------------------------------------------------------------
+
+  it should "handle a single quoted parameter with a single character" in {
+    val result: Option[ParsedFunctionCall] = FunctionCallParser.parseFunctionCall("""fn("a")""")
+    result.isDefined should be(true)
+    result.get should be(ParsedFunctionCall(functionName = "fn",
+      params = Seq(Parameter(tpe = ParameterType.QUOTED_STRING, value = "a"))))
+  }
+
+  it should "handle a single quoted parameter with two characters" in {
+    val result: Option[ParsedFunctionCall] = FunctionCallParser.parseFunctionCall("""fn("ab")""")
+    result.isDefined should be(true)
+    result.get should be(ParsedFunctionCall(functionName = "fn",
+      params = Seq(Parameter(tpe = ParameterType.QUOTED_STRING, value = "ab"))))
+  }
+
+  it should "handle a single quoted parameter with three characters" in {
+    val result: Option[ParsedFunctionCall] = FunctionCallParser.parseFunctionCall("""fn("abc")""")
+    result.isDefined should be(true)
+    result.get should be(ParsedFunctionCall(functionName = "fn",
+      params = Seq(Parameter(tpe = ParameterType.QUOTED_STRING, value = "abc"))))
+  }
+
+  it should "handle a single quoted parameter with leading whitespace" in {
+    val result: Option[ParsedFunctionCall] = FunctionCallParser.parseFunctionCall("""fn( "abc")""")
+    result.isDefined should be(true)
+    result.get should be(ParsedFunctionCall(functionName = "fn",
+      params = Seq(Parameter(tpe = ParameterType.QUOTED_STRING, value = "abc"))))
+  }
+
+  it should "handle a single quoted parameter with trailing whitespace" in {
+    val result: Option[ParsedFunctionCall] = FunctionCallParser.parseFunctionCall("""fn("abc" )""")
+    result.isDefined should be(true)
+    result.get should be(ParsedFunctionCall(functionName = "fn",
+      params = Seq(Parameter(tpe = ParameterType.QUOTED_STRING, value = "abc"))))
+  }
+
+  it should "handle a single quoted parameter with an escaped quotation mark" in {
+    val result: Option[ParsedFunctionCall] = FunctionCallParser.parseFunctionCall("""fn("ab\"")""")
+    result.isDefined should be(true)
+    result.get should be(ParsedFunctionCall(functionName = "f",
+      params = Seq(Parameter(tpe = ParameterType.QUOTED_STRING, value = "ab\""))))
+  }
+
+  it should "handle a single quoted parameter with an escaped quotation mark with a character after" in {
+    val result: Option[ParsedFunctionCall] = FunctionCallParser.parseFunctionCall("""fn("ab\"c")""")
+    result.isDefined should be(true)
+    result.get should be(ParsedFunctionCall(functionName = "f",
+      params = Seq(Parameter(tpe = ParameterType.QUOTED_STRING, value = "ab\"c"))))
+  }
+
+  // Literal and numeric arguments
+  // -------------------------------------------------------------------------------------------------------------------
+
+  it should "handle a single numeric and a single literal argument" in {
+    val result: Option[ParsedFunctionCall] = FunctionCallParser.parseFunctionCall("""fn(2,ab)""")
+    result.isDefined should be (true)
+    result.get should be (ParsedFunctionCall(functionName = "fn",
+      params = Seq(
+        Parameter(tpe = ParameterType.NUMERIC, value = "2"),
+        Parameter(tpe = ParameterType.LITERAL, value = "ab"))))
+  }
+
+  it should "handle a single numeric and a single literal argument with whitespace" in {
+    val result: Option[ParsedFunctionCall] = FunctionCallParser.parseFunctionCall("""fn(2, ab)""")
+    result.isDefined should be (true)
+    result.get should be (ParsedFunctionCall(functionName = "fn",
+      params = Seq(
+        Parameter(tpe = ParameterType.NUMERIC, value = "2"),
+        Parameter(tpe = ParameterType.LITERAL, value = "ab"))))
+  }
+
+  // Error cases
+  // -------------------------------------------------------------------------------------------------------------------
+
+  it should "return None if the function name starts with a space" in {
+    val result: Option[ParsedFunctionCall] = FunctionCallParser.parseFunctionCall(""" a()""")
+    result.isDefined should be (false)
+  }
+
+  it should "return None if the function name starts with a digit" in {
+    val result: Option[ParsedFunctionCall] = FunctionCallParser.parseFunctionCall("""21()""")
+    result.isDefined should be (false)
+  }
+
+  it should "return None if the function name starts with a ." in {
+    val result: Option[ParsedFunctionCall] = FunctionCallParser.parseFunctionCall(""".1()""")
+    result.isDefined should be (false)
+  }
+
+  it should "return None if the function doesn't have an open bracket" in {
+    val result: Option[ParsedFunctionCall] = FunctionCallParser.parseFunctionCall("""f""")
+    result.isDefined should be (false)
+  }
+
+  it should "return None if the function doesn't have a close bracket" in {
+    val result: Option[ParsedFunctionCall] = FunctionCallParser.parseFunctionCall("""f(""")
+    result.isDefined should be (false)
+  }
+
+  it should "return None if the function doesn't have a close bracket after whitespace" in {
+    val result: Option[ParsedFunctionCall] = FunctionCallParser.parseFunctionCall("""f( """)
+    result.isDefined should be (false)
+  }
+
+  it should "return None if the function has a close bracket without an open" in {
+    val result: Option[ParsedFunctionCall] = FunctionCallParser.parseFunctionCall("""f)""")
+    result.isDefined should be (false)
+  }
+
+//  it should "return None if two numeric arguments aren't separated with a comma" in {
+//    val result: Option[ParsedFunctionCall] = FunctionCallParser.parseFunctionCall("""f(1 2)""")
+//    result.isDefined should be (false)
+//  }
+
+
+
+}
